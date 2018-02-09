509d33fc2ba58800bd4f52d44bbe7bf9826ad4c0
William <WarMad2@Hotmail.com>
2018-02-09 14:57:24 -0500

    Created Cat extends VirtualPet Class through CatTest Class

---
+++ src/main/java/virtualPetShelter/Cat.java
@@ -0,0 +1,9 @@
+package virtualPetShelter;
+
+public class Cat extends VirtualPet {
+
+	public Cat(String name, String description, int hunger, int water, int boredom) {
+		super(name, description, hunger, water, boredom);
+	}
+
+}
--- src/test/java/virtualPetShelter/CatTest.java
+++ src/test/java/virtualPetShelter/CatTest.java
@@ -1,5 +1,41 @@
 package virtualPetShelter;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class CatTest {
+
+	VirtualPet underTest = new Cat("Tara", "Is a cat", 20, 30, 40);
+
+	@Test
+	public void shouldReturnCatName() {
+		String check = underTest.getName();
+		assertEquals(check, "Tara");
+	}
+
+	@Test
+	public void shouldReturnCatDescription() {
+		String check = underTest.getDescription();
+		assertEquals(check, "Is a cat");
+	}
+
+	@Test
+	public void shouldReturnCatHunger() {
+		int check = underTest.getHunger();
+		assertEquals(check, 20);
+	}
+
+	@Test
+	public void shouldReturnCatWater() {
+		int check = underTest.getWater();
+		assertEquals(check, 30);
+	}
+
+	@Test
+	public void shouldReturnCatBoredom() {
+		int check = underTest.getBoredom();
+		assertEquals(check, 40);
+	}
 
 }
