a122ea67d35c9fcd50938674c0957c4fe0cc89bf
William <WarMad2@Hotmail.com>
2018-02-10 14:50:39 -0500

    Created RobotDog extends RobotPet Class through RobotDogTest class.

---
+++ src/main/java/virtualPetShelter/RobotDog.java
@@ -0,0 +1,10 @@
+package virtualPetShelter;
+
+public class RobotDog extends RobotPet {
+
+	public RobotDog(String name, String description, int oil, int batteryLevel, int happiness) {
+		super(name, description, oil, batteryLevel, happiness);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/test/java/virtualPetShelter/RobotDogTest.java
+++ src/test/java/virtualPetShelter/RobotDogTest.java
@@ -1,5 +1,6 @@
 package virtualPetShelter;
 
 public class RobotDogTest {
+	RobotPet underTest = new RobotDog("Jax", "description", 40, 50, 60);
 
 }
