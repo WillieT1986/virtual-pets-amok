1dc9efaba079293fcdc771e9c3094caac71634a8
William <WarMad2@Hotmail.com>
2018-02-10 14:56:17 -0500

    Copied tdd from DogTest to RobotDogTest, commented out everything and testing one by one to be sure that name, description, oil, batteryLevel and happiness is returned.

--- src/main/java/virtualPetShelter/RobotDog.java
+++ src/main/java/virtualPetShelter/RobotDog.java
@@ -4,7 +4,6 @@ public class RobotDog extends RobotPet {
 
 	public RobotDog(String name, String description, int oil, int batteryLevel, int happiness) {
 		super(name, description, oil, batteryLevel, happiness);
-		// TODO Auto-generated constructor stub
 	}
 
 }
--- src/test/java/virtualPetShelter/RobotDogTest.java
+++ src/test/java/virtualPetShelter/RobotDogTest.java
@@ -1,6 +1,52 @@
 package virtualPetShelter;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class RobotDogTest {
 	RobotPet underTest = new RobotDog("Jax", "description", 40, 50, 60);
+
+	@Test
+	public void shouldReturnDogName() {
+		String check = underTest.getName();
+		assertEquals(check, "Hold");
+	}
+
+	// @Test
+	// public void shouldReturnDogDescription() {
+	// String check = underTest.getDescription();
+	// assertEquals(check, "Is a dog");
+	// }
+	//
+	// @Test
+	// public void shouldReturnDogHunger() {
+	// int check = underTest.getHunger();
+	// assertEquals(check, 20);
+	// }
+	//
+	// @Test
+	// public void shouldReturnDogWater() {
+	// int check = underTest.getWater();
+	// assertEquals(check, 30);
+	// }
+	//
+	// @Test
+	// public void shouldReturnDogBoredom() {
+	// int check = underTest.getBoredom();
+	// assertEquals(check, 40);
+	// }
+	//
+	// @Test
+	// public void shouldReturnDogWaste() {
+	// int check = underTest.getWaste();
+	// assertEquals(check, 50);
+	// }
+	//
+	// @Test
+	// public void shouldReturnDogHappiness() {
+	// int check = underTest.getHappiness();
+	// assertEquals(check, 40);
+	// }
 
 }
