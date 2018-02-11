88b1a846b409026fd3cbd6d66065e511fbe5444b
William <WarMad2@Hotmail.com>
2018-02-11 14:28:05 -0500

    Created DogKennel Class through TDD.

--- src/test/java/virtualPetShelter/DogKennelTest.java
+++ src/test/java/virtualPetShelter/DogKennelTest.java
@@ -1,5 +1,14 @@
 package virtualPetShelter;
 
+import org.junit.Before;
+
 public class DogKennelTest {
+
+	private DogKennel underTest;
+
+	@Before
+	public void setUp() {
+		underTest = new DogKennel();
+	}
 
 }
