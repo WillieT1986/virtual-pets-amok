04c0e96711aac323b9f15d7633ff4494ec8b021f
William <WarMad2@Hotmail.com>
2018-02-11 14:40:59 -0500

    Deleting DogKennelTest Class & DogKennel Class. Merging dog kenneling with VirtualPet Class.

--- src/main/java/virtualPetShelter/DogKennel.java
+++ src/main/java/virtualPetShelter/DogKennel.java
@@ -2,4 +2,18 @@ package virtualPetShelter;
 
 public class DogKennel {
 
+	private int wasteLevel;
+
+	public DogKennel() {
+		this.(0);
+	}
+
+	public DogKennel(flithy) {
+		wasteLevel = filthy;
+	}
+
+	public int getWasteLevel() {
+		return wasteLevel;
+	}
+
 }
--- src/test/java/virtualPetShelter/DogKennelTest.java
+++ src/test/java/virtualPetShelter/DogKennelTest.java
@@ -1,6 +1,10 @@
 package virtualPetShelter;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Before;
+import org.junit.Test;
 
 public class DogKennelTest {
 
@@ -9,6 +13,19 @@ public class DogKennelTest {
 	@Before
 	public void setUp() {
 		underTest = new DogKennel();
+	}
+
+	@Test
+	public void instantiateDogKennel() {
+		underTest = new DogKennel();
+		assertNotNull(underTest);
+	}
+
+	@Test
+	public void shouldGetWasteLevel() {
+		int check = underTest.getWasteLevel();
+
+		assertEquals(check, 1);
 	}
 
 }
