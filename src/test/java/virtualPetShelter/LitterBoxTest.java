3ec81123434561c2f0df6a6bfa11bd6c84073c22
William <WarMad2@Hotmail.com>
2018-02-11 04:08:47 -0500

    Instantiated LitterBox Class

--- src/test/java/virtualPetShelter/LitterBoxTest.java
+++ src/test/java/virtualPetShelter/LitterBoxTest.java
@@ -1,5 +1,22 @@
 package virtualPetShelter;
 
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class LitterBoxTest {
+
+	private LitterBox underTest;
+
+	@Before
+	public void setUp() {
+		underTest = new LitterBox();
+	}
+
+	@Test
+	public void instantiateLitterBox() {
+		assertNotNull(underTest);
+	}
 
 }
